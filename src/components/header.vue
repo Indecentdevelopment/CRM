51ca63f69b92c9cd1b581e50c0d62b4c4e175649
zhuqitao <1362335135@qq.com>
2018-03-16 09:45:54 +0800

    朱启涛： 项目环境初始配置

---
+++ src/components/header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header">
+        header
+    </div>
+</template>
+<script>
+import "@/assets/components/header.sass"
+export default {
+    data: function () {
+        return {
+        }
+    }
+}
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="index">
+
+        <!-- 头部 顶部 -->
+        <my-header></my-header>
+
+        <!-- 切换城市 -->
+        <div class="city"></div>
+
+        <!-- 内容部分 -->
+        <div class="content">
+            
+        </div>
+    </div>
+</template>
+<script>
+import Header from '@/components/header'
+export default {
+    data: function () {
+        return {
+          
+        }
+    },
+    methodes:{
+
+    },
+    components:{
+        myHeader: Header
+    }
+}
+</script>
