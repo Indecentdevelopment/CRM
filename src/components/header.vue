0b3756e5955ffbb328a2f8938ec112b591fff418
‘MyGuowei’ <982172220@qq.com>
2018-03-16 18:08:52 +0800

    ‘header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="header">
-        header
+    	<img src="../assets/images/header/back.png" class="h-back"/>
+    	<img src="../assets/images/header/home-btn.png" class="h-home"/>
+    	<img src="../assets/images/header/refresh.png" class="h-refresh"/>
+    	<img src="../assets/images/header/logo-black-constant.png" class="h-logo"/>
+        <span class="h-landingstate">
+        	<span id="passCity" class="passCity">银川西</span>
+			<span id="cancellation" class="cancellation">注销</span>
+        </span>
     </div>
 </template>
 <script>
@@ -8,6 +15,7 @@ import "@/assets/components/header.sass"
 export default {
     data: function () {
         return {
+        	
         }
     }
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,20 +5,35 @@
         <my-header></my-header>
 
         <!-- 切换城市 -->
-        <div class="city"></div>
+        <div class="city clearfix">
+        	<div class="city-btn fr">
+				<span class="">
+					当前城市：
+				</span>
+				<span class="currentCity">
+					{{sgm}}
+				</span>
+				<span class="switch" @click="citySwitching">
+					[切换]
+				</span>
+			</div>
+        </div>
 
         <!-- 内容部分 -->
         <div class="content">
-            
+            <div class="">
+            	
+            </div>
         </div>
     </div>
 </template>
 <script>
 import Header from '@/components/header'
+import "./index.sass"
 export default {
     data: function () {
         return {
-          
+          sgm: '青岛市'
         }
     },
     methodes:{
