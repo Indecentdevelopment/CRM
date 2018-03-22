c8bfecf62a8c6e11df54d758fa8cf88637df9ad4
‘MyGuowei’ <982172220@qq.com>
2018-03-22 18:10:11 +0800

    serverList

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import App from './App'
 import router from './router'
 
 import store from './vuex/index'
+//import login from './pages/login/login'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
 
 import './config/index'
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+Vue.use(ElementUI);
 new Vue({
   el: '#app',
   router,
