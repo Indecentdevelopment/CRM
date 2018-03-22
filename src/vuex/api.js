1734330d68a8d27a85590ae937c8800fbc01ac20
zhuqitao <1362335135@qq.com>
2018-03-22 19:09:36 +0800

    home-获取carInfo数据

--- src/vuex/api.js
+++ src/vuex/api.js
@@ -2,6 +2,23 @@ const querystring = require('querystring')
 
 let url = process.env.NODE_ENV !== 'production' ? '/Api/' : 'http://act.tirecool.net/Api/'
 
+// http 请求request 拦截器
+axios.interceptors.request.use(
+    config => {
+        if (localStorage.access_token) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
+            config.headers.Authorization = `${JSON.parse(localStorage.token_type)} ${JSON.parse(localStorage.access_token)}`
+        } else {
+            // this.$router.push('/login')
+        }
+        return config
+    },
+    err => {
+        return Promise.reject(err)
+    }
+)
+// http 响应response 拦截器
+
+
 export default {
    /***************************首页**********************************/
 
--- src/vuex/modules/login.js
+++ src/vuex/modules/login.js
@@ -8,20 +8,46 @@ const state = {
     }
 }
 const actions = {
-    setCookie: function (name,value,expires) {
-        var exdate=new Date(expires)
-        document.cookie=name+ "=" +escape(value)+
-        ((expires==null) ? "" : ";expires="+exdate.toGMTString())
+    setCookie: function ({commit}, data) {
+        return (document.cookie = [
+            data.key, '=', escape(data.value),
+            data.options.expires ? '; expires=' + data.options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+            data.options.path    ? '; path=' + data.options.path : '',
+            data.options.domain  ? '; domain=' + data.options.domain : '',
+            data.options.secure  ? '; secure' : ''
+        ].join(''));
     },
     toLogin: ({dispatch, commit}, data) => {
         api.toLogin(data).then(res => {
             // commit(type.GET_SHOPNAMELIST, res.data)
             if (res.status === 200) {
-                dispatch('setCookie', 'access_token', res.data['access_token'], res.data['.expires'])
-                dispatch('setCookie', 'token_type', res.data['token_type'], res.data['.expires'])
-                dispatch('setCookie', 'shopId', res.data['shopId'], res.data['.expires'])
-                dispatch('setCookie', 'shopName', res.data['shopName'], res.data['.expires'])
-                dispatch('setCookie', 'isStaff', res.data['isStaff'], res.data['.expires'])
+                localStorage.setItem('access_token', JSON.stringify(res.data['access_token']));
+                localStorage.setItem('token_type', JSON.stringify(res.data['token_type']));
+                // dispatch('setCookie', {
+                //     key: 'access_token',
+                //     value: res.data['access_token'],
+                //     options: { path: '/', expires: new Date(res.data['.expires']) }
+                // })
+                // dispatch('setCookie', {
+                //     key: 'token_type',
+                //     value: res.data['token_type'],
+                //     options: { path: '/', expires: new Date(res.data['.expires']) }
+                // })
+                // dispatch('setCookie', {
+                //     key: 'shopId',
+                //     value: res.data['shopId'],
+                //     options: { path: '/', expires: new Date(res.data['.expires']) }
+                // })
+                // dispatch('setCookie', {
+                //     key: 'shopName',
+                //     value: res.data['shopName'],
+                //     options: { path: '/', expires: new Date(res.data['.expires']) }
+                // })
+                // dispatch('setCookie', {
+                //     key: 'isStaff',
+                //     value: res.data['isStaff'],
+                //     options: { path: '/', expires: new Date(res.data['.expires']) }
+                // })
                 commit(type.SET_ISLOGIN, true)
                 commit(type.SET_SHOPDATA, data.shopData)
                 router.push('/home')
