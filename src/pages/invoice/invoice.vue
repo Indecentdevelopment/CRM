ae865a48269e31d6108a677484befcdf3563726f
‘MyGuowei’ <982172220@qq.com>
2018-04-26 18:07:38 +0800

    发票

--- src/pages/invoice/invoice.vue
+++ src/pages/invoice/invoice.vue
@@ -4,16 +4,16 @@
     	<!-- 头部 顶部 -->
         <my-header></my-header>
         <div class="userSelection clearfix">
-        	<p class="fl">总金额：<b>{{total}}</b></p>
+        	<p class="fl">总金额：<b>{{totalTotal}}</b></p>
         	<span class="fr">开票</span>
         	<input type="text" class="fr" />
         </div>
         
         <div class="userData clearfix" v-for="item in userDetails">
         	<div class="useLeft fl">
-        		<input type="checkbox" :id="item.orderId" :value="item.orderId" name="total" v-model="total"/>
+        		<input type="checkbox" :id="item.price" :value="item.price" name="total" v-model="total" @change="totalTotalFn(total)"/>
         	</div>
-        	<div class="useRighr fr" >
+        	<div class="useRighr fr">
         		<p class="fl">姓名：{{item.name}}</p>
         		<p class="fl">手机号：{{item.mobile}}</p>
         		<p class="fl">车牌号：{{item.carNo}}</p>
@@ -34,7 +34,8 @@
                 isLoading: true,  		// loading
                 uid: '',			    //获取userInfo时  传参
                 userDetails: '',		//用户信息
-                total: []				//总额
+                total: [],				//总额数组
+                totalTotal: 0			//总额求和
 	        }
 	    },
 	    created() {
@@ -50,6 +51,20 @@
                 	this.userDetails = res.data.repairsModel
                 	console.log(res.data)
                 })
+	    	},
+	    	totalTotalFn(i){
+	    		console.log(i)
+	    		if(i <= 0){
+	    			if(this.total.length == 0){
+		    			this.totalTotal = 0
+		    		}
+		    		if(this.totalTotal == 0){
+		    			this.totalTotal = 0
+		    		}
+	    		}else{
+	    			this.totalTotal = parseInt(this.totalTotal) + parseInt(this.total.slice(-1))
+	    		}
+	    		
 	    	}
 	    },
 	    components:{
