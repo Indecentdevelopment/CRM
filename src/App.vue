7af0c999a8b6cb8daae47be12407cb38610e50d9
zhuqitao <1362335135@qq.com>
2018-05-21 18:28:43 +0800

    版本控制

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
   <div id="app">
     <router-view/>
+
+    <div class="version" v-show="isUpdateVersion">
+        <div class="update">
+            <div class="info">
+                有最新版本，是否更新？
+            </div>
+            <button class="sure-update" @click="updateVersion">确定</button>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 import '@/assets/sass/common.sass'
 import '@/assets/sass/iconfont.css'
+import { mapGetters } from 'vuex'
 export default {
     name: 'App',
+    data () {
+        return {}
+    },
     created () {
-        // this.$router.push('/home')
-        this.$store.dispatch('checkToken')
+        
+        document.addEventListener('plusready', () => {
+            var oldVersion = plus.runtime.version
+            this.$store.dispatch('setOldVersion', oldVersion)
+            this.$store.dispatch('getNewVersion')
+            this.$store.dispatch('checkToken')
+        });
+
+        // var oldVersion = '0.0'
+        // this.$store.dispatch('setOldVersion', oldVersion)
+        // this.$store.dispatch('getNewVersion')
+        // this.$store.dispatch('checkToken')
+           
+        
+    },
+    computed: {
+        ...mapGetters({
+            isUpdateVersion: 'isUpdateVersion'
+        })
+    },
+    methods: {
+        updateVersion () {
+            var u = navigator.userAgent;
+            var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+            var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+            if (isAndroid) {
+                plus.runtime.openURL('http://crm.tirecool.net/crm.apk')
+            } else if (isiOS) {
+                plus.runtime.openURL('https://www.pgyer.com/tirecoolwms')
+            }
+            
+        }
     }
 }
 </script>
--- src/vuex/index.js
+++ src/vuex/index.js
@@ -3,13 +3,15 @@ import Vuex from 'vuex'
 
 import Index from './modules/index' // 首页vuex模块
 import Login from './modules/login'
+import allocation from './modules/allocation'
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
-  modules: {
-    Index,
-    Login
-  }
+	modules: {
+		Index,
+		Login,
+		allocation
+	}
 })
 
 export default store
